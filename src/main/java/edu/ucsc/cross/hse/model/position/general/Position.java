eb8426a553556447412793a2cf7d5f76c2aa7071
be3short <beshort@ucsc.edu>
2017-08-13 20:42:38 -0700

    Implemented position calculation methods

--- src/main/java/edu/ucsc/cross/hse/model/position/general/Position.java
+++ src/main/java/edu/ucsc/cross/hse/model/position/general/Position.java
@@ -22,4 +22,24 @@ public interface Position
 	 * Flag indicating if the position is null
 	 */
 	public boolean isNullPosition();
+
+	public static Double computeDirectDistance(Position a, Position b)
+	{
+		Double distance = Math.sqrt(Math.pow((a.getXPosition() - b.getXPosition()), 2)
+		+ Math.pow((a.getYPosition() - b.getYPosition()), 2) + Math.pow((a.getZPosition() - b.getZPosition()), 2));
+		return distance;
+	}
+
+	public static Double computeHorizontalDistance(Position a, Position b)
+	{
+		Double distance = Math
+		.sqrt(Math.pow((a.getXPosition() - b.getXPosition()), 2) + Math.pow((a.getYPosition() - b.getYPosition()), 2));
+		return distance;
+	}
+
+	public static Double computeVerticalDistance(Position a, Position b)
+	{
+		Double distance = a.getZPosition() - b.getZPosition();
+		return distance;
+	}
 }
